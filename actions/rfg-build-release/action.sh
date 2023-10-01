309d356be66e901add1e5e98d09e8e146e899585
kvieta <kvieta@localhost>
2023-10-01 17:09:22 +0000

    hmm

--- actions/rfg-build-release/action.sh
+++ actions/rfg-build-release/action.sh
@@ -17,8 +17,8 @@ git config --global --add safe.directory /github/workspace
 # handle absolute version
 if [ "$absver" = "1" ]; then
   # calc absolute version, zero pad to 5 digits. RELEASE_VERSION has to be set for RFG.
-  RELEASE_VERSION=$(printf "%05d" $(git rev-list --count HEAD))
-  VERSION="$RELEASE_VERSION" # might work as override?
+  RELEASE_VERSION='c'$(printf "%05d" $(git rev-list --count HEAD))
+#  VERSION="$RELEASE_VERSION" # i think this overrides the version in RFG. at least, it seems to.
   TAG_PRETTY=$(git rev-list --count HEAD)
   echo "using absolute release version #$RELEASE_VERSION"
 fi
@@ -41,9 +41,9 @@ curl -fsSL https://raw.githubusercontent.com/GTNewHorizons/GTNH-Actions-Workflow
 echo ""
 
 #rename jars with absolute version number
-#if [ "$absver" = "1" ]; then
-#  prename 's/^(.*?)-/$1-'"$RELEASE_VERSION"'-/' *
-#fi
+if [ "$absver" = "1" ]; then
+  prename 's/^(.*?)-/$1-'"$RELEASE_VERSION"'-/' *
+fi
 
 #get name for release (uses shortest jar filename)
 RELEASENAME=$(find ./build/libs -type f -name "*.jar" | awk '{print length, $0}' | sort -n | head -n 1 | cut -d " " -f 2- | sed 's/\.jar$//' | xargs -I{} basename {})
