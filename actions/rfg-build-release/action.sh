7da5ec3d1310880db5d056e23f0e084be4e301c4
kvieta <kvieta@localhost>
2023-10-01 16:52:04 +0000

    update naming

--- actions/rfg-build-release/action.sh
+++ actions/rfg-build-release/action.sh
@@ -16,8 +16,10 @@ git config --global --add safe.directory /github/workspace
 
 # handle absolute version
 if [ "$absver" = "1" ]; then
-  # calc absolute version, zero pad to 5 digits. value of VERSION is used by RFG as override.
+  # calc absolute version, zero pad to 5 digits. RELEASE_VERSION has to be set for RFG.
   RELEASE_VERSION=$(printf "%05d" $(git rev-list --count HEAD))
+  VERSION="$RELEASE_VERSION" # might work as override?
+  TAG_PRETTY=$(git rev-list --count HEAD)
   echo "using absolute release version #$RELEASE_VERSION"
 fi
 
@@ -38,9 +40,14 @@ timeout 120 ./gradlew --build-cache --info --stacktrace runServer 2>&1 < run/sto
 curl -fsSL https://raw.githubusercontent.com/GTNewHorizons/GTNH-Actions-Workflows/master/scripts/test_no_error_reports | bash
 echo ""
 
+#rename jars with absolute version number
+#if [ "$absver" = "1" ]; then
+#  prename 's/^(.*?)-/$1-'"$RELEASE_VERSION"'-/' *
+#fi
+
 #get name for release (uses shortest jar filename)
-RELEASENAME=$(find ./build/libs -type f -name "*.jar" | awk '{print length, $0}' | sort -n | head -n 1 | cut -d " " -f 2- | sed 's/\.jar$// | xargs -I{} basename {}')
+RELEASENAME=$(find ./build/libs -type f -name "*.jar" | awk '{print length, $0}' | sort -n | head -n 1 | cut -d " " -f 2- | sed 's/\.jar$//' | xargs -I{} basename {})
 echo "release = $RELEASENAME"
 
 #create github release with artifacts
-gh release create "$RELEASE_VERSION" ./build/libs/*.jar --generate-notes --title "$RELEASE_VERSION"
+gh release create "$TAG_PRETTY" ./build/libs/*.jar --generate-notes --title "$RELEASENAME"
